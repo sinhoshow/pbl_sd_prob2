7808bc20fe16f78dd01d46c6859774343db494b7
Biancasantana1 <50915572+Biancasantana1@users.noreply.github.com>
2022-05-02 15:15:02 -0300

    Update clockDiv.v

--- clockDiv.v
+++ clockDiv.v
@@ -35,9 +35,9 @@ module clockDiv(
 	reg [25:0] COUNTER;
 	initial COUNTER = 0;
 	
-	assign OUT1 = COUNTER[2];  //bit 3 do contador --- 2.5MHz
-	assign OUT2 = COUNTER[9];  //bit 10 do contador --- 19.5KHz
-	assign OUT3 = COUNTER[23]; //bit 24 do contador --- 1.19Hz
+	assign OUT1 = COUNTER[2];  //bit 3 do contador --- 6.25MHz
+	assign OUT2 = COUNTER[9];  //bit 10 do contador --- 48.9KHz
+	assign OUT3 = COUNTER[23]; //bit 24 do contador --- 2.98Hz
 	
 	always @(posedge CLK)
 	begin
